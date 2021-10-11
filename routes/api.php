d8fbb7cc87b1561f25c30fb3c587b2c69b7309c8
alexandr <sajke.shevchenko@gmail.com>
2021-10-11 15:47:35 +0700

    Add api routes

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\TariffController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/orders', [OrderController::class, 'index']);
+Route::post('/orders', [OrderController::class, 'store']);
+Route::get('/orders/{order}', [OrderController::class, 'show']);
+Route::get('/tariffs', [TariffController::class, 'index']);
+Route::get('/tariffs/{tariff}', [TariffController::class, 'show']);
